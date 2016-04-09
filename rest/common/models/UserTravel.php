73c17fdca8c31969596075e43336a8807043815d
Jordi Corominas <jcorominas@ingeniummobile.com>
2016-04-09 20:43:21 +0200

    Afegit usertravel

---
+++ rest/common/models/UserTravel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use \common\models\base\UserTravel as BaseUserTravel;
+
+/**
+ * This is the model class for table "UserTravel".
+ */
+class UserTravel extends BaseUserTravel
+{
+}
