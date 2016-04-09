d1609df01591e35227886e7c20379525f1a3d7f3
Xan Torres <xan.torres@gmail.com>
2016-04-09 20:56:18 +0200

    Merge branch 'master' of https://github.com/hackathongi2016/travel
    Conflicts:
    	front/app/scripts/app.js
    	front/app/scripts/controllers/detail.js
    	front/app/scripts/controllers/new.js

--- front/app/scripts/controllers/detail.js
+++ front/app/scripts/controllers/detail.js
@@ -10,41 +10,59 @@
 angular.module('trabelApp')
   .controller('DetailCtrl', function (userId, Restangular, $routeParams, $scope) {
 
-    Restangular.one('travels',$routeParams.travelId).get()
-      .then(function(travel){
-        $scope.travel = travel;
+    var me = this;
+    var dateFields = ["tra_date", "tra_planning_limit"];
+    var fieldsToDate = function (model) {
+      return _.transform(model, function (model, v, k) {
+        if (_.includes(dateFields, k)) {
+          model[k] = new Date(v);
+        }
+      }, model);
+    };
+    var datesToString = function (model) {
+      return _.transform(model, function (model, v, k) {
+        if (_.includes(dateFields, k)) {
+          model[k] = moment(v).format('YYYY-MM-DD');
+        }
+      }, model);
+    };
+    var travel = {};
+    Restangular.one('travels', $routeParams.travelId).get({expand: "topics,users"})
+      .then(function (travelData) {
+        travel = $scope.travel = fieldsToDate(travelData);
+        $scope.topics = travel.topics || [];
+        $scope.users = travel.users || [];
+        $scope.userIsAdmin = userId == travel.tra_usr_id;
+        //$scope.$apply();
       });
+    $scope.userIsAdmin = false;
+
+    $scope.$watch('travel.tra_persons_max', function (newValue) {
+      if (newValue < travel.tra_persons_min) {
+        travel.tra_persons_max = travel.tra_persons_min;
+      }
+    });
+
+    $scope.$watch('travel.tra_budget_max', function (newValue) {
+      if (newValue < travel.tra_budget_min) {
+        travel.tra_budget_max = travel.tra_budget_min;
+      }
+    });
 
     /*
-    Restangular.one('users').getList();
-    Restangular.one('topics').getList();
-    Restangular.one('proposals').getList();
-    */
+     Restangular.one('users').getList();
+     Restangular.one('topics').getList();
+     Restangular.one('proposals').getList();
+     */
     //Restangular.one('travels',$routeParams.travelId).get()();
-    var travel = $scope.travel = {
-      tra_origin: "Barcelona",
-      tra_destination: "Berlin",
-      tra_num_days: 3,
-      tra_budget_min: 500,
-      tra_budget_max: 1500,
-      tra_date: moment("2016-08-08").toDate(),
-      tra_planning_limit: moment("2016-06-08").toDate(),
-      tra_persons_min: 0,
-      tra_persons_max: 1,
-      tra_description: null,
-      tra_usr_id: 1
-    };
 
-    $scope.userIsAdmin = userId == travel.tra_usr_id;
-    var me = this;
-
-    $scope.join = function(){
+    $scope.join = function () {
 
     };
 
-    var userIds = _.map(users,"usr_id");
-    $scope.userIsTraveler = function(){
-      return _.includes(userIds,userId);
+    var userIds = _.map(users, "usr_id");
+    $scope.userIsTraveler = function () {
+      return _.includes(userIds, userId);
     };
 
     $scope.editMode = false;
@@ -75,22 +93,22 @@ angular.module('trabelApp')
         });
     };
 
-    $scope.delete = function(){
+    $scope.delete = function () {
       alert("TODO");
     };
 
     var users = $scope.users = [
       {
-        usr_id: 1,
-        usr_name: "Pepito",
-        usr_surname: "Grillo",
-        usr_nickname: "",
-        usr_mail: "",
-        usr_password: "",
-        usr_birthday: "",
-        usr_gender: "",
+        usr_id           : 1,
+        usr_name         : "Pepito",
+        usr_surname      : "Grillo",
+        usr_nickname     : "",
+        usr_mail         : "",
+        usr_password     : "",
+        usr_birthday     : "",
+        usr_gender       : "",
         usr_register_date: "",
-        usr_avatar_url: ""
+        usr_avatar_url   : ""
       }
     ];
     $scope.topics = [
@@ -144,7 +162,7 @@ angular.module('trabelApp')
     $scope.temes = {};
     $scope.temes.newtema = '';
     $scope.temes.defaults = [
-      {text:'Allotjament', done:true},
-      {text:'Desplaçament', done:false}
+      {text: 'Allotjament', done: true},
+      {text: 'Desplaçament', done: false}
     ];
   });
