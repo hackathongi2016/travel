b54345c91e4b11c861ce83b1b3d1fa85247f21eb
Xan Torres <xan.torres@gmail.com>
2016-04-09 12:15:48 +0200

    Creació del projecte part frontend

---
+++ front/app/scripts/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name trabelApp
+ * @description
+ * # trabelApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('trabelApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/create', {
+        templateUrl: 'views/create.html',
+        controller: 'CreateCtrl',
+        controllerAs: 'create'
+      })
+      .when('/detail', {
+        templateUrl: 'views/detail.html',
+        controller: 'DetailCtrl',
+        controllerAs: 'detail'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
