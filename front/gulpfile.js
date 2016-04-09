fb3d128d27c2532be694ba2073168ca0fa434a1c
Xan Torres <xan.torres@gmail.com>
2016-04-09 12:59:47 +0200

    Afegides dependencies i mogut index de la vista a l'arrel d'app

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -30,7 +30,7 @@ var paths = {
   ],
   karma: 'karma.conf.js',
   views: {
-    main: yeoman.app + '/index.html',
+    main: yeoman.app + '/views/index.html',
     files: [yeoman.app + '/views/**/*.html']
   }
 };
